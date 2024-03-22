e41a061dc7b8eaf2bcb1af911d953bf5cb02c7e7
EdzusB <edzusbleikss1@gmail.com>
2024-03-22 22:42:16 +0200

    Pievienota izvade

--- Form1.cs
+++ Form1.cs
@@ -28,17 +28,44 @@ namespace Palidzibas_serviss
             // Find the index of the maximum probability in the prediction array
             int maxIndex = 0;
             float maxProbability = prediction.Score[0];
+
             for (int i = 1; i < prediction.Score.Length; i++)
             {
                 if (prediction.Score[i] > maxProbability)
                 {
                     maxProbability = prediction.Score[i];
-                    maxIndex = i;
+                    maxIndex = i+1;
                 }
             }
 
             // Assuming maxIndex corresponds to the category label
-            cena.Text = maxIndex.ToString(); // Display the predicted category label
+            string predictedCategory = $"Category {maxIndex}"; // Adjust according to your category labels
+
+            Console.WriteLine("MaxIndex: " + maxIndex);
+            Console.WriteLine("Prediction Scores: " + string.Join(", ", prediction.Score));
+
+            if (maxIndex == 0)
+            {
+                MessageBox.Show("Jūsu ziņa nosūtīta IT nozarei!");
+            }
+            else if (maxIndex == 2)
+            {
+                MessageBox.Show("Jūsu ziņa nosūtīta Finanšu nozarei!");
+            }
+            else if (maxIndex == 3)
+            {
+                MessageBox.Show("Jūsu ziņa nosūtīta Pārdošanas nozarei!");
+            }
+            else if (maxIndex == 4)
+            {
+                MessageBox.Show("Jūsu ziņa nosūtīta Cilvēkresursu nozarei!");
+            }
+            else if (maxIndex == 5)
+            {
+                MessageBox.Show("Jūsu ziņa nosūtīta Mārketinga nozarei!");
+            }
+
+            //cena.Text = predictedCategory; // Display the predicted category label
         }
     }
 }
